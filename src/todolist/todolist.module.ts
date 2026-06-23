16a41f950fa13a3d198ba5a5bfb0f09e72ebf295
satlihana_p <satlihanapetriti@gmail.com>
2026-06-23 17:49:25 +0200

    add missing command and query handlers for todos module

--- src/todolist/todolist.module.ts
+++ src/todolist/todolist.module.ts
@@ -6,13 +6,20 @@ import { TodoEntity } from './Entity/todo.entity';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreateTodoHandler } from './commands/handlers/create-todo.handler';
 import { GetTodoByIdHandler } from './queries/handlers/get-todo-by-id.handler';
+import { UpdateTodoHandler } from './commands/handlers/update-todo.handler';
+import { DeleteTodoHandler } from './commands/handlers/delete-todo.handler';
+import { GetAllTodosHandler } from './queries/handlers/get-all-todos.handler';
 
 const CommandHandlers = [
   CreateTodoHandler,
+  DeleteTodoHandler,
+  UpdateTodoHandler,
 ];
 
 const QueryHandlers = [
   GetTodoByIdHandler,
+  GetAllTodosHandler,
+
 ];
 
 @Module({
